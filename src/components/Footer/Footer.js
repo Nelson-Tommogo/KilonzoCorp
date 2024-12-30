c392ef42b5d8b5949ac5d6b3920f29502b0b7757
Nelson-Tommogo <nelsontommogo9@gmail.com>
2024-12-30 13:26:51 +0300

    kilonzocorp

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -113,16 +113,6 @@ const Footer = () => {
                             <p className={styles.text}>
                                 PO Box 2288-90100 Machakos
                             </p>
-                            <p className={styles.text}>
-                                <a 
-                                    href="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3988.394398688508!2d37.275028174966195!3d-1.5307949984548723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMcKwMzEnNTAuOSJTIDM3wrAxNiczOS40IkU!5e0!3m2!1sen!2ske!4v1735552202036!5m2!1sen!2ske" 
-                                    target="_blank" 
-                                    rel="noopener noreferrer"
-                                    style={{ color: '#fff', textDecoration: 'underline' }}
-                                >
-                                    View Location on Google Maps
-                                </a>
-                            </p>
                         </div>
                     </div>
                 </div>
